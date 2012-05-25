fd0c6bfbe7aba8839c2ee63b8f9de7a01ec00960
nidnarb <nidnarb@gmail.com>
2012-05-24 20:17:41 -0700

    Update src/com/massivecraft/factions/cmd/CmdHelp.java

--- src/com/massivecraft/factions/cmd/CmdHelp.java
+++ src/com/massivecraft/factions/cmd/CmdHelp.java
@@ -37,7 +37,7 @@ public class CmdHelp extends FCommand
 		
 		int page = this.argAsInt(0, 1);
 		
-		sendMessage(p.txt.titleize("Factions Help ("+page+"/"+helpPages.size()+")"));
+		sendMessage(p.txt.titleize("Nommix Guild Help ("+page+"/"+helpPages.size()+")"));
 		
 		page -= 1;
 		
@@ -68,7 +68,7 @@ public class CmdHelp extends FCommand
 		pageLines.add( p.cmdBase.cmdJoin.getUseageTemplate(true) );
 		pageLines.add( p.cmdBase.cmdLeave.getUseageTemplate(true) );
 		pageLines.add( p.cmdBase.cmdHome.getUseageTemplate(true) );
-		pageLines.add( p.txt.parse("<i>Learn how to create a faction on the next page.") );
+		pageLines.add( p.txt.parse("<i>Learn how to create a guild on the next page.") );
 		helpPages.add(pageLines);
 		
 		pageLines = new ArrayList<String>();
@@ -86,7 +86,7 @@ public class CmdHelp extends FCommand
 		if (Econ.isSetup() && Conf.econEnabled && Conf.bankEnabled)
 		{
 			pageLines = new ArrayList<String>();
-			pageLines.add( p.txt.parse("<i>Your faction has a bank which is used to pay for certain" ));
+			pageLines.add( p.txt.parse("<i>Your Guild has a bank which is used to pay for certain" ));
 			pageLines.add( p.txt.parse("<i>things, so it will need to have money deposited into it." ));
 			pageLines.add( p.txt.parse("<i>To learn more, use the money command." ));
 			pageLines.add( "" );
@@ -106,15 +106,15 @@ public class CmdHelp extends FCommand
 		pageLines.add( p.cmdBase.cmdOfficer.getUseageTemplate(true) );
 		pageLines.add( p.cmdBase.cmdLeader.getUseageTemplate(true) );
 		pageLines.add( p.cmdBase.cmdTitle.getUseageTemplate(true) );
-		pageLines.add( p.txt.parse("<i>Player titles are just for fun. No rules connected to them." ));
+		pageLines.add( p.txt.parse("<i>Player titles are just for fun. Use your creativity" ));
 		helpPages.add(pageLines);
 		
 		pageLines = new ArrayList<String>();
 		pageLines.add( p.cmdBase.cmdMap.getUseageTemplate(true) );
 		pageLines.add( p.cmdBase.cmdSeeChunks.getUseageTemplate(true) );
 		pageLines.add(p.txt.parse("<i>Claimed land with ownership set is further protected so"));
-		pageLines.add(p.txt.parse("<i>that only the owner(s), faction admin, and possibly the"));
-		pageLines.add(p.txt.parse("<i>faction moderators have full access."));
+		pageLines.add(p.txt.parse("<i>that only the guild masters, guild admin, and possibly the"));
+		pageLines.add(p.txt.parse("<i>guild moderators have full access."));
 		helpPages.add(pageLines);
 		
 		pageLines = new ArrayList<String>();
@@ -124,26 +124,26 @@ public class CmdHelp extends FCommand
 		pageLines.add( p.cmdBase.cmdRelationTruce.getUseageTemplate(true) ); 
 		pageLines.add( p.cmdBase.cmdRelationNeutral.getUseageTemplate(true) );
 		pageLines.add( p.cmdBase.cmdRelationEnemy.getUseageTemplate(true) );
-		pageLines.add(p.txt.parse("<i>Set the relation you WISH to have with another faction."));
-		pageLines.add(p.txt.parse("<i>Your default relation with other factions will be neutral."));
-		pageLines.add(p.txt.parse("<i>If BOTH factions choose \"ally\" you will be allies."));
-		pageLines.add(p.txt.parse("<i>If ONE faction chooses \"enemy\" you will be enemies."));
+		pageLines.add(p.txt.parse("<i>Set the relation you WISH to have with another guild."));
+		pageLines.add(p.txt.parse("<i>Your default relation with other guilds will be neutral."));
+		pageLines.add(p.txt.parse("<i>If BOTH guilds choose \"ally\" you will be allies."));
+		pageLines.add(p.txt.parse("<i>If ONE guild chooses \"enemy\" you will be enemies."));
 		helpPages.add(pageLines);
 		
 		pageLines = new ArrayList<String>();
 		pageLines.add(p.txt.parse("<i>You can never hurt members or allies."));
 		pageLines.add(p.txt.parse("<i>You can not hurt neutrals in their own territory."));
-		pageLines.add(p.txt.parse("<i>You can always hurt enemies and players without faction."));
+		pageLines.add(p.txt.parse("<i>You can always hurt enemies and players without a guild."));
 		pageLines.add("");
 		pageLines.add(p.txt.parse("<i>Damage from enemies is reduced in your own territory."));
 		pageLines.add(p.txt.parse("<i>When you die you lose power. It is restored over time."));
-		pageLines.add(p.txt.parse("<i>The power of a faction is the sum of all member power."));
-		pageLines.add(p.txt.parse("<i>The power of a faction determines how much land it can hold."));
-		pageLines.add(p.txt.parse("<i>You can claim land from factions with too little power."));
+		pageLines.add(p.txt.parse("<i>The power of a guild is the sum of all member power."));
+		pageLines.add(p.txt.parse("<i>The power of a guild determines how much land it can hold."));
+		pageLines.add(p.txt.parse("<i>You can claim land from guilds with too little power."));
 		helpPages.add(pageLines);
 		
 		pageLines = new ArrayList<String>();
-		pageLines.add(p.txt.parse("<i>Only faction members can build and destroy in their own"));
+		pageLines.add(p.txt.parse("<i>Only guild members can build and destroy in their own"));
 		pageLines.add(p.txt.parse("<i>territory. Usage of the following items is also restricted:"));
 		pageLines.add(p.txt.parse("<i>Door, Chest, Furnace, Dispenser, Diode."));
 		pageLines.add("");
@@ -155,7 +155,7 @@ public class CmdHelp extends FCommand
 		helpPages.add(pageLines);
 		
 		pageLines = new ArrayList<String>();
-		pageLines.add("Finally some commands for the server admins:");
+		pageLines.add("Some Commands for our admins:");
 		pageLines.add( p.cmdBase.cmdBypass.getUseageTemplate(true) );
 		pageLines.add(p.txt.parse("<c>/f claim safezone <i>claim land for the Safe Zone"));
 		pageLines.add(p.txt.parse("<c>/f claim warzone <i>claim land for the War Zone"));
