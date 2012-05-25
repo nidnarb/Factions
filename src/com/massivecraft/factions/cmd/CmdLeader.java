b863b7b5360731284bc3c882f9d0caa903dbb48f
nidnarb <nidnarb@gmail.com>
2012-05-24 21:13:46 -0700

    Update src/com/massivecraft/factions/cmd/CmdLeader.java

--- src/com/massivecraft/factions/cmd/CmdLeader.java
+++ src/com/massivecraft/factions/cmd/CmdLeader.java
@@ -1,14 +1,14 @@
-package com.massivecraft.factions.cmd;
+package com.massivecraft.guilds.cmd;
 
 import org.bukkit.Bukkit;
 
-import com.massivecraft.factions.FPlayer;
-import com.massivecraft.factions.FPlayers;
-import com.massivecraft.factions.Faction;
-import com.massivecraft.factions.event.FPlayerJoinEvent;
-import com.massivecraft.factions.struct.Permission;
-import com.massivecraft.factions.struct.Rel;
-import com.massivecraft.factions.util.RelationUtil;
+import com.massivecraft.guilds.FPlayer;
+import com.massivecraft.guilds.FPlayers;
+import com.massivecraft.guilds.guild;
+import com.massivecraft.guilds.event.FPlayerJoinEvent;
+import com.massivecraft.guilds.struct.Permission;
+import com.massivecraft.guilds.struct.Rel;
+import com.massivecraft.guilds.util.RelationUtil;
 
 public class CmdLeader extends FCommand
 {	
@@ -18,7 +18,7 @@ public class CmdLeader extends FCommand
 		this.aliases.add("leader");
 		
 		this.requiredArgs.add("player");
-		this.optionalArgs.put("faction", "your");
+		this.optionalArgs.put("guild", "your");
 		
 		this.permission = Permission.LEADER.node;
 		this.disableOnLock = true;
@@ -35,12 +35,12 @@ public class CmdLeader extends FCommand
 		FPlayer newLeader = this.argAsBestFPlayerMatch(0);
 		if (newLeader == null) return;
 		
-		Faction targetFaction = this.argAsFaction(1, myFaction);
-		if (targetFaction == null) return;
+		guild targetguild = this.argAsguild(1, myguild);
+		if (targetguild == null) return;
 		
-		FPlayer targetFactionCurrentLeader = targetFaction.getFPlayerLeader();
+		FPlayer targetguildCurrentLeader = targetguild.getFPlayerLeader();
 		
-		// We now have fplayer and the target faction
+		// We now have fplayer and the target guild
 		if (this.senderIsConsole || fme.hasAdminMode() || Permission.LEADER_ANY.has(sender, false))
 		{
 			// Do whatever you wish
@@ -48,15 +48,15 @@ public class CmdLeader extends FCommand
 		else
 		{
 			// Follow the standard rules
-			if (fme.getRole() != Rel.LEADER || targetFaction != myFaction)
+			if (fme.getRole() != Rel.LEADER || targetguild != myguild)
 			{
-				sender.sendMessage(p.txt.parse("<b>You must be leader of the faction to %s.", this.getHelpShort()));
+				sender.sendMessage(p.txt.parse("<b>You must be leader of the guild to %s.", this.getHelpShort()));
 				return;
 			}
 			
-			if (newLeader.getFaction() != myFaction)
+			if (newLeader.getguild() != myguild)
 			{
-				msg("%s<i> is not a member in the faction.", newLeader.describeTo(fme, true));
+				msg("%s<i> is not a member in the guild.", newLeader.describeTo(fme, true));
 				return;
 			}
 			
@@ -67,36 +67,36 @@ public class CmdLeader extends FCommand
 			}
 		}
 
-		// only perform a FPlayerJoinEvent when newLeader isn't actually in the faction
-		if (newLeader.getFaction() != targetFaction)
+		// only perform a FPlayerJoinEvent when newLeader isn't actually in the guild
+		if (newLeader.getguild() != targetguild)
 		{
-			FPlayerJoinEvent event = new FPlayerJoinEvent(FPlayers.i.get(me),targetFaction,FPlayerJoinEvent.PlayerJoinReason.LEADER);
+			FPlayerJoinEvent event = new FPlayerJoinEvent(FPlayers.i.get(me),targetguild,FPlayerJoinEvent.PlayerJoinReason.LEADER);
 			Bukkit.getServer().getPluginManager().callEvent(event);
 			if (event.isCancelled()) return;
 		}
 
 		// if target player is currently leader, demote and replace him
-		if (targetFactionCurrentLeader == newLeader)
+		if (targetguildCurrentLeader == newLeader)
 		{
-			targetFaction.promoteNewLeader();
-			msg("<i>You have demoted %s<i> from the position of faction leader.", newLeader.describeTo(fme, true));
-			newLeader.msg("<i>You have been demoted from the position of faction leader by %s<i>.", senderIsConsole ? "a server admin" : fme.describeTo(newLeader, true));
+			targetguild.promoteNewLeader();
+			msg("<i>You have demoted %s<i> from the position of guild leader.", newLeader.describeTo(fme, true));
+			newLeader.msg("<i>You have been demoted from the position of guild leader by %s<i>.", senderIsConsole ? "a server admin" : fme.describeTo(newLeader, true));
 			return;
 		}
 
 		// Perform the switching
-		if (targetFactionCurrentLeader != null)
+		if (targetguildCurrentLeader != null)
 		{
-			targetFactionCurrentLeader.setRole(Rel.OFFICER);
+			targetguildCurrentLeader.setRole(Rel.OFFICER);
 		}
-		newLeader.setFaction(targetFaction);
+		newLeader.setguild(targetguild);
 		newLeader.setRole(Rel.LEADER);
-		msg("<i>You have promoted %s<i> to the position of faction leader.", newLeader.describeTo(fme, true));
+		msg("<i>You have promoted %s<i> to the position of guild leader.", newLeader.describeTo(fme, true));
 		
 		// Inform all players
 		for (FPlayer fplayer : FPlayers.i.getOnline())
 		{
-			fplayer.msg("%s<i> gave %s<i> the leadership of %s<i>.", senderIsConsole ? "A server admin" : RelationUtil.describeThatToMe(fme, fplayer, true), newLeader.describeTo(fplayer), targetFaction.describeTo(fplayer));
+			fplayer.msg("%s<i> gave %s<i> the leadership of %s<i>.", senderIsConsole ? "A server admin" : RelationUtil.describeThatToMe(fme, fplayer, true), newLeader.describeTo(fplayer), targetguild.describeTo(fplayer));
 		}
 	}
 }
