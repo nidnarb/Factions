03c0c4975fff0976f2efddc4e932bd0b5457773d
nidnarb <nidnarb@gmail.com>
2012-05-24 21:12:43 -0700

    Update src/com/massivecraft/factions/cmd/CmdJoin.java

--- src/com/massivecraft/factions/cmd/CmdJoin.java
+++ src/com/massivecraft/factions/cmd/CmdJoin.java
@@ -1,14 +1,14 @@
-package com.massivecraft.factions.cmd;
+package com.massivecraft.guilds.cmd;
 
 import org.bukkit.Bukkit;
 
-import com.massivecraft.factions.Conf;
-import com.massivecraft.factions.FPlayers;
-import com.massivecraft.factions.Faction;
-import com.massivecraft.factions.FPlayer;
-import com.massivecraft.factions.P;
-import com.massivecraft.factions.event.FPlayerJoinEvent;
-import com.massivecraft.factions.struct.Permission;
+import com.massivecraft.guilds.Conf;
+import com.massivecraft.guilds.FPlayers;
+import com.massivecraft.guilds.guild;
+import com.massivecraft.guilds.FPlayer;
+import com.massivecraft.guilds.P;
+import com.massivecraft.guilds.event.FPlayerJoinEvent;
+import com.massivecraft.guilds.struct.Permission;
 
 public class CmdJoin extends FCommand
 {
@@ -17,7 +17,7 @@ public class CmdJoin extends FCommand
 		super();
 		this.aliases.add("join");
 		
-		this.requiredArgs.add("faction");
+		this.requiredArgs.add("guild");
 		this.optionalArgs.put("player", "you");
 		
 		this.permission = Permission.JOIN.node;
@@ -32,77 +32,77 @@ public class CmdJoin extends FCommand
 	@Override
 	public void perform()
 	{
-		Faction faction = this.argAsFaction(0);
-		if (faction == null) return;
+		guild guild = this.argAsguild(0);
+		if (guild == null) return;
 
 		FPlayer fplayer = this.argAsBestFPlayerMatch(1, fme, false);
 		boolean samePlayer = fplayer == fme;
 
 		if (!samePlayer  && ! Permission.JOIN_OTHERS.has(sender, false))
 		{
-			msg("<b>You do not have permission to move other players into a faction.");
+			msg("<b>You do not have permission to move other players into a guild.");
 			return;
 		}
 
-		if (faction == fplayer.getFaction())
+		if (guild == fplayer.getguild())
 		{
-			msg("<b>%s %s already a member of %s", fplayer.describeTo(fme, true), (samePlayer ? "are" : "is"), faction.getTag(fme));
+			msg("<b>%s %s already a member of %s", fplayer.describeTo(fme, true), (samePlayer ? "are" : "is"), guild.getTag(fme));
 			return;
 		}
 
-		if (Conf.factionMemberLimit > 0 && faction.getFPlayers().size() >= Conf.factionMemberLimit)
+		if (Conf.guildMemberLimit > 0 && guild.getFPlayers().size() >= Conf.guildMemberLimit)
 		{
-			msg(" <b>!<white> The faction %s is at the limit of %d members, so %s cannot currently join.", faction.getTag(fme), Conf.factionMemberLimit, fplayer.describeTo(fme, false));
+			msg(" <b>!<white> The guild %s is at the limit of %d members, so %s cannot currently join.", guild.getTag(fme), Conf.guildMemberLimit, fplayer.describeTo(fme, false));
 			return;
 		}
 
-		if (fplayer.hasFaction())
+		if (fplayer.hasguild())
 		{
-			msg("<b>%s must leave %s current faction first.", fplayer.describeTo(fme, true), (samePlayer ? "your" : "their"));
+			msg("<b>%s must leave %s current guild first.", fplayer.describeTo(fme, true), (samePlayer ? "your" : "their"));
 			return;
 		}
 
 		if (!Conf.canLeaveWithNegativePower && fplayer.getPower() < 0)
 		{
-			msg("<b>%s cannot join a faction with a negative power level.", fplayer.describeTo(fme, true));
+			msg("<b>%s cannot join a guild with a negative power level.", fplayer.describeTo(fme, true));
 			return;
 		}
 
-		if( ! (faction.getOpen() || faction.isInvited(fplayer) || fme.hasAdminMode() || Permission.JOIN_ANY.has(sender, false)))
+		if( ! (guild.getOpen() || guild.isInvited(fplayer) || fme.hasAdminMode() || Permission.JOIN_ANY.has(sender, false)))
 		{
-			msg("<i>This faction requires invitation.");
+			msg("<i>This guild requires invitation.");
 			if (samePlayer)
-				faction.msg("%s<i> tried to join your faction.", fplayer.describeTo(faction, true));
+				guild.msg("%s<i> tried to join your guild.", fplayer.describeTo(guild, true));
 			return;
 		}
 
 		// if economy is enabled, they're not on the bypass list, and this command has a cost set, make sure they can pay
-		if (samePlayer && ! canAffordCommand(Conf.econCostJoin, "to join a faction")) return;
+		if (samePlayer && ! canAffordCommand(Conf.econCostJoin, "to join a guild")) return;
 
 		// trigger the join event (cancellable)
-		FPlayerJoinEvent joinEvent = new FPlayerJoinEvent(FPlayers.i.get(me),faction,FPlayerJoinEvent.PlayerJoinReason.COMMAND);
+		FPlayerJoinEvent joinEvent = new FPlayerJoinEvent(FPlayers.i.get(me),guild,FPlayerJoinEvent.PlayerJoinReason.COMMAND);
 		Bukkit.getServer().getPluginManager().callEvent(joinEvent);
 		if (joinEvent.isCancelled()) return;
 
 		// then make 'em pay (if applicable)
-		if (samePlayer && ! payForCommand(Conf.econCostJoin, "to join a faction", "for joining a faction")) return;
+		if (samePlayer && ! payForCommand(Conf.econCostJoin, "to join a guild", "for joining a guild")) return;
 
-		fme.msg("<i>%s successfully joined %s.", fplayer.describeTo(fme, true), faction.getTag(fme));
+		fme.msg("<i>%s successfully joined %s.", fplayer.describeTo(fme, true), guild.getTag(fme));
 
 		if (!samePlayer)
-			fplayer.msg("<i>%s moved you into the faction %s.", fme.describeTo(fplayer, true), faction.getTag(fplayer));
-		faction.msg("<i>%s joined your faction.", fplayer.describeTo(faction, true));
+			fplayer.msg("<i>%s moved you into the guild %s.", fme.describeTo(fplayer, true), guild.getTag(fplayer));
+		guild.msg("<i>%s joined your guild.", fplayer.describeTo(guild, true));
 
-		fplayer.resetFactionData();
-		fplayer.setFaction(faction);
-		faction.deinvite(fplayer);
+		fplayer.resetguildData();
+		fplayer.setguild(guild);
+		guild.deinvite(fplayer);
 
-		if (Conf.logFactionJoin)
+		if (Conf.logguildJoin)
 		{
 			if (samePlayer)
-				P.p.log("%s joined the faction %s.", fplayer.getName(), faction.getTag());
+				P.p.log("%s joined the guild %s.", fplayer.getName(), guild.getTag());
 			else
-				P.p.log("%s moved the player %s into the faction %s.", fme.getName(), fplayer.getName(), faction.getTag());
+				P.p.log("%s moved the player %s into the guild %s.", fme.getName(), fplayer.getName(), guild.getTag());
 		}
 	}
 }
