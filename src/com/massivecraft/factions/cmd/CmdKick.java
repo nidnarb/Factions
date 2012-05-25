d5a5b312aac42b8c79070f177eb6531f9beab56d
nidnarb <nidnarb@gmail.com>
2012-05-24 21:13:13 -0700

    Update src/com/massivecraft/factions/cmd/CmdKick.java

--- src/com/massivecraft/factions/cmd/CmdKick.java
+++ src/com/massivecraft/factions/cmd/CmdKick.java
@@ -1,15 +1,15 @@
-package com.massivecraft.factions.cmd;
+package com.massivecraft.guilds.cmd;
 
 import org.bukkit.Bukkit;
 
-import com.massivecraft.factions.Conf;
-import com.massivecraft.factions.FPlayer;
-import com.massivecraft.factions.Faction;
-import com.massivecraft.factions.P;
-import com.massivecraft.factions.event.FPlayerLeaveEvent;
-import com.massivecraft.factions.struct.FPerm;
-import com.massivecraft.factions.struct.Permission;
-import com.massivecraft.factions.struct.Rel;
+import com.massivecraft.guilds.Conf;
+import com.massivecraft.guilds.FPlayer;
+import com.massivecraft.guilds.guild;
+import com.massivecraft.guilds.P;
+import com.massivecraft.guilds.event.FPlayerLeaveEvent;
+import com.massivecraft.guilds.struct.FPerm;
+import com.massivecraft.guilds.struct.Permission;
+import com.massivecraft.guilds.struct.Rel;
 
 public class CmdKick extends FCommand
 {
@@ -56,36 +56,36 @@ public class CmdKick extends FCommand
 			return;
 		}
 		
-		Faction yourFaction = you.getFaction();
+		guild yourguild = you.getguild();
 
-		if (fme != null && ! FPerm.KICK.has(fme, yourFaction)) return;
+		if (fme != null && ! FPerm.KICK.has(fme, yourguild)) return;
 
 		// if economy is enabled, they're not on the bypass list, and this command has a cost set, make sure they can pay
-		if ( ! canAffordCommand(Conf.econCostKick, "to kick someone from the faction")) return;
+		if ( ! canAffordCommand(Conf.econCostKick, "to kick someone from the guild")) return;
 
 		// trigger the leave event (cancellable) [reason:kicked]
-		FPlayerLeaveEvent event = new FPlayerLeaveEvent(you, you.getFaction(), FPlayerLeaveEvent.PlayerLeaveReason.KICKED);
+		FPlayerLeaveEvent event = new FPlayerLeaveEvent(you, you.getguild(), FPlayerLeaveEvent.PlayerLeaveReason.KICKED);
 		Bukkit.getServer().getPluginManager().callEvent(event);
 		if (event.isCancelled()) return;
 
 		// then make 'em pay (if applicable)
-		if ( ! payForCommand(Conf.econCostKick, "to kick someone from the faction", "for kicking someone from the faction")) return;
+		if ( ! payForCommand(Conf.econCostKick, "to kick someone from the guild", "for kicking someone from the guild")) return;
 
-		yourFaction.msg("%s<i> kicked %s<i> from the faction! :O", fme.describeTo(yourFaction, true), you.describeTo(yourFaction, true));
-		you.msg("%s<i> kicked you from %s<i>! :O", fme.describeTo(you, true), yourFaction.describeTo(you));
-		if (yourFaction != myFaction)
+		yourguild.msg("%s<i> kicked %s<i> from the guild! :O", fme.describeTo(yourguild, true), you.describeTo(yourguild, true));
+		you.msg("%s<i> kicked you from %s<i>! :O", fme.describeTo(you, true), yourguild.describeTo(you));
+		if (yourguild != myguild)
 		{
-			fme.msg("<i>You kicked %s<i> from the faction %s<i>!", you.describeTo(fme), yourFaction.describeTo(fme));
+			fme.msg("<i>You kicked %s<i> from the guild %s<i>!", you.describeTo(fme), yourguild.describeTo(fme));
 		}
 
-		if (Conf.logFactionKick)
-			P.p.log((senderIsConsole ? "A console command" : fme.getName())+" kicked "+you.getName()+" from the faction: "+yourFaction.getTag());
+		if (Conf.logguildKick)
+			P.p.log((senderIsConsole ? "A console command" : fme.getName())+" kicked "+you.getName()+" from the guild: "+yourguild.getTag());
 
 		if (you.getRole() == Rel.LEADER)
-			yourFaction.promoteNewLeader();
+			yourguild.promoteNewLeader();
 
-		yourFaction.deinvite(you);
-		you.resetFactionData();
+		yourguild.deinvite(you);
+		you.resetguildData();
 	}
 	
 }
