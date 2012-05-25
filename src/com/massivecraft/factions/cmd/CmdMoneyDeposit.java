049e28e4b73b153fb08ad55340210182a26d3b9d
nidnarb <nidnarb@gmail.com>
2012-05-24 21:17:23 -0700

    Update src/com/massivecraft/factions/cmd/CmdMoneyDeposit.java

--- src/com/massivecraft/factions/cmd/CmdMoneyDeposit.java
+++ src/com/massivecraft/factions/cmd/CmdMoneyDeposit.java
@@ -1,10 +1,10 @@
-package com.massivecraft.factions.cmd;
+package com.massivecraft.guilds.cmd;
 
-import com.massivecraft.factions.Conf;
-import com.massivecraft.factions.P;
-import com.massivecraft.factions.iface.EconomyParticipator;
-import com.massivecraft.factions.integration.Econ;
-import com.massivecraft.factions.struct.Permission;
+import com.massivecraft.guilds.Conf;
+import com.massivecraft.guilds.P;
+import com.massivecraft.guilds.iface.EconomyParticipator;
+import com.massivecraft.guilds.integration.Econ;
+import com.massivecraft.guilds.struct.Permission;
 
 import org.bukkit.ChatColor;
 
@@ -19,7 +19,7 @@ public class CmdMoneyDeposit extends FCommand
 		this.aliases.add("deposit");
 		
 		this.requiredArgs.add("amount");
-		this.optionalArgs.put("faction", "your");
+		this.optionalArgs.put("guild", "your");
 		
 		this.permission = Permission.MONEY_DEPOSIT.node;
 		this.setHelpShort("deposit money");
@@ -34,12 +34,12 @@ public class CmdMoneyDeposit extends FCommand
 	public void perform()
 	{
 		double amount = this.argAsDouble(0, 0d);
-		EconomyParticipator faction = this.argAsFaction(1, myFaction);
-		if (faction == null) return;
-		boolean success = Econ.transferMoney(fme, fme, faction, amount);
+		EconomyParticipator guild = this.argAsguild(1, myguild);
+		if (guild == null) return;
+		boolean success = Econ.transferMoney(fme, fme, guild, amount);
 
 		if (success && Conf.logMoneyTransactions)
-			P.p.log(ChatColor.stripColor(P.p.txt.parse("%s deposited %s in the faction bank: %s", fme.getName(), Econ.moneyString(amount), faction.describeTo(null))));
+			P.p.log(ChatColor.stripColor(P.p.txt.parse("%s deposited %s in the guild bank: %s", fme.getName(), Econ.moneyString(amount), guild.describeTo(null))));
 	}
 	
 }
