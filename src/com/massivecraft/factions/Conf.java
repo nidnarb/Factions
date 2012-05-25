661268d66056de04b8aa1bd6ae2188b151dd49cf
nidnarb <nidnarb@gmail.com>
2012-05-24 21:30:55 -0700

    Update src/com/massivecraft/factions/Conf.java

--- src/com/massivecraft/factions/Conf.java
+++ src/com/massivecraft/factions/Conf.java
@@ -1,13 +1,13 @@
-package com.massivecraft.factions;
+package com.massivecraft.guilds;
 
 import java.util.*;
 
 import org.bukkit.*;
 import org.bukkit.entity.EntityType;
 
-import com.massivecraft.factions.struct.FFlag;
-import com.massivecraft.factions.struct.FPerm;
-import com.massivecraft.factions.struct.Rel;
+import com.massivecraft.guilds.struct.FFlag;
+import com.massivecraft.guilds.struct.FPerm;
+import com.massivecraft.guilds.struct.Rel;
 
 public class Conf
 {
@@ -25,9 +25,9 @@ public class Conf
 	public static ChatColor colorFriendlyFire = ChatColor.DARK_RED;
 	//public static ChatColor colorWilderness = ChatColor.DARK_GREEN;
 	
-	public static Map<FFlag, Boolean> factionFlagDefaults;
-	public static Map<FFlag, Boolean> factionFlagIsChangeable;
-	public static Map<FPerm, Set<Rel>> factionPermDefaults;
+	public static Map<FFlag, Boolean> guildFlagDefaults;
+	public static Map<FFlag, Boolean> guildFlagIsChangeable;
+	public static Map<FPerm, Set<Rel>> guildPermDefaults;
 	
 	// Power
 	public static double powerPlayerMax = 10.0;
@@ -40,33 +40,33 @@ public class Conf
 	public static boolean powerRegenOffline = false;  // does player power regenerate even while they're offline?
 	public static double powerOfflineLossPerDay = 0.0;  // players will lose this much power per day offline
 	public static double powerOfflineLossLimit = 0.0;  // players will no longer lose power from being offline once their power drops to this amount or less
-	public static double powerFactionMax = 0.0;  // if greater than 0, the cap on how much power a faction can have (additional power from players beyond that will act as a "buffer" of sorts)
+	public static double powerguildMax = 0.0;  // if greater than 0, the cap on how much power a guild can have (additional power from players beyond that will act as a "buffer" of sorts)
 	
 	public static String prefixLeader = "**";
 	public static String prefixOfficer = "*";
 	
-	public static int factionTagLengthMin = 3;
-	public static int factionTagLengthMax = 10;
-	public static boolean factionTagForceUpperCase = false;
+	public static int guildTagLengthMin = 3;
+	public static int guildTagLengthMax = 10;
+	public static boolean guildTagForceUpperCase = false;
 	
-	public static boolean newFactionsDefaultOpen = false;
+	public static boolean newguildsDefaultOpen = false;
 
-	// when faction membership hits this limit, players will no longer be able to join using /f join; default is 0, no limit
-	public static int factionMemberLimit = 0;
+	// when guild membership hits this limit, players will no longer be able to join using /f join; default is 0, no limit
+	public static int guildMemberLimit = 0;
 
-	// what faction ID to start new players in when they first join the server; default is 0, "no faction"
-	public static String newPlayerStartingFactionID = "0";
+	// what guild ID to start new players in when they first join the server; default is 0, "no guild"
+	public static String newPlayerStartingguildID = "0";
 
-	//public static boolean showMapFactionKey = true;
-	//public static boolean showNeutralFactionsOnMap = true;
-	//public static boolean showEnemyFactionsOnMap = true;
+	//public static boolean showMapguildKey = true;
+	//public static boolean showNeutralguildsOnMap = true;
+	//public static boolean showEnemyguildsOnMap = true;
 	
 	// Disallow joining/leaving/kicking while power is negative
 	public static boolean canLeaveWithNegativePower = true;
 	
-	// Configuration on the Faction tag in chat messages.
+	// Configuration on the guild tag in chat messages.
 	public static boolean chatSetFormat = false;
-	public static String chatSetFormatTo = "<{faction_relcolor}§l{faction_roleprefix}§r{faction_relcolor}{faction_tag_pr}"+ChatColor.WHITE.toString()+"%s> %s";
+	public static String chatSetFormatTo = "<{guild_relcolor}§l{guild_roleprefix}§r{guild_relcolor}{guild_tag_pr}"+ChatColor.WHITE.toString()+"%s> %s";
 	public static boolean chatParseTags = true;
 	public static boolean chatParseTagsColored = true;
 	public static Map<String, String> chatSingleFormats = new HashMap<String, String>();
@@ -74,7 +74,7 @@ public class Conf
 	public static String chatTagFormat = "%s"+ChatColor.WHITE; // This one is almost deprecated now right? or is it?
 	
 	// Herochat
-	public static String herochatFactionChannelName = "Faction";
+	public static String herochatguildChannelName = "guild";
 	public static String herochatAllyChannelName = "Allies";
 	
 	public static double autoLeaveAfterDaysOfInactivity = 10.0;
@@ -89,11 +89,11 @@ public class Conf
 	public static boolean onCaptureResetLwcLocks = false;
 
 	// server logging options
-	public static boolean logFactionCreate = true;
-	public static boolean logFactionDisband = true;
-	public static boolean logFactionJoin = true;
-	public static boolean logFactionKick = true;
-	public static boolean logFactionLeave = true;
+	public static boolean logguildCreate = true;
+	public static boolean logguildDisband = true;
+	public static boolean logguildJoin = true;
+	public static boolean logguildKick = true;
+	public static boolean logguildLeave = true;
 	public static boolean logLandClaims = true;
 	public static boolean logLandUnclaims = true;
 	public static boolean logMoneyTransactions = true;
@@ -119,37 +119,37 @@ public class Conf
 	
 	
 	public static Rel friendlyFireFromRel = Rel.TRUCE;
-	public static boolean disablePVPForFactionlessPlayers = false;
-	public static boolean enablePVPAgainstFactionlessInAttackersLand = false;
+	public static boolean disablePVPForguildlessPlayers = false;
+	public static boolean enablePVPAgainstguildlessInAttackersLand = false;
 	
 	public static int noPVPDamageToOthersForXSecondsAfterLogin = 3;
 
 	//public static boolean peacefulMembersDisablePowerLoss = true;
 	
-	public static boolean permanentFactionsDisableLeaderPromotion = false;
+	public static boolean permanentguildsDisableLeaderPromotion = false;
 	
 	public static boolean claimsMustBeConnected = false;
-	public static boolean claimsCanBeUnconnectedIfOwnedByOtherFaction = true;
-	public static int claimsRequireMinFactionMembers = 1;
+	public static boolean claimsCanBeUnconnectedIfOwnedByOtherguild = true;
+	public static int claimsRequireMinguildMembers = 1;
 	public static int claimedLandsMax = 0;
 
 	// if someone is doing a radius claim and the process fails to claim land this many times in a row, it will exit
 	public static int radiusClaimFailureLimit = 9;
 
-	//public static double considerFactionsReallyOfflineAfterXMinutes = 0.0;
+	//public static double considerguildsReallyOfflineAfterXMinutes = 0.0;
 	
 	public static int actionDeniedPainAmount = 2;
 
-	// commands which will be prevented if the player is a member of a permanent faction
-	public static Set<String> permanentFactionMemberDenyCommands = new LinkedHashSet<String>();
+	// commands which will be prevented if the player is a member of a permanent guild
+	public static Set<String> permanentguildMemberDenyCommands = new LinkedHashSet<String>();
 
-	// commands which will be prevented when in claimed territory of another faction
+	// commands which will be prevented when in claimed territory of another guild
 	public static Set<String> territoryNeutralDenyCommands = new LinkedHashSet<String>();
 	public static Set<String> territoryEnemyDenyCommands = new LinkedHashSet<String>();
 	
 	public static double territoryShieldFactor = 0.3;
 
-	// for claimed areas where further faction-member ownership can be defined
+	// for claimed areas where further guild-member ownership can be defined
 
 	public static boolean pistonProtectionThroughDenyBuild = true;
 
@@ -165,8 +165,8 @@ public class Conf
 	public static transient Set<EntityType> monsters = EnumSet.noneOf(EntityType.class);
 
 	// Spout features
-	public static boolean spoutFactionTagsOverNames = true;  // show faction tags over names over player heads
-	public static boolean spoutFactionTitlesOverNames = true;  // whether to include player's title in that
+	public static boolean spoutguildTagsOverNames = true;  // show guild tags over names over player heads
+	public static boolean spoutguildTitlesOverNames = true;  // whether to include player's title in that
 	public static boolean spoutHealthBarUnderNames = true;  // Show healthbar under player names.
 	public static String spoutHealthBarLeft = "{c}[";
 	public static String spoutHealthBarSolid = "|";
@@ -177,14 +177,14 @@ public class Conf
 	public static String spoutHealthBarColorTag = "{c}";
 	public static int spoutHealthBarWidth = 30;
 	public static Map<Double, String> spoutHealthBarColorUnderQuota = new LinkedHashMap<Double, String>();
-	public static boolean spoutCapes = true;  // Show faction capes
+	public static boolean spoutCapes = true;  // Show guild capes
 	public static int spoutTerritoryDisplayPosition = 1;  // permanent territory display, instead of by chat; 0 = disabled, 1 = top left, 2 = top center, 3+ = top right
 	public static float spoutTerritoryDisplaySize = 1.0f;  // text scale (size) for territory display
-	public static boolean spoutTerritoryDisplayShowDescription = true;  // whether to show the faction description, not just the faction tag
+	public static boolean spoutTerritoryDisplayShowDescription = true;  // whether to show the guild description, not just the guild tag
 	public static boolean spoutTerritoryAccessShow = true;  // show occasional territory access info as well ("access granted" or "access restricted" if relevant)
 	public static boolean spoutTerritoryNoticeShow = true;  // show additional brief territory notice near center of screen, to be sure player notices transition
 	public static int spoutTerritoryNoticeTop = 40;  // how far down the screen to place the additional notice
-	public static boolean spoutTerritoryNoticeShowDescription = false;  // whether to show the faction description in the notice, not just the faction tag
+	public static boolean spoutTerritoryNoticeShowDescription = false;  // whether to show the guild description in the notice, not just the guild tag
 	public static float spoutTerritoryNoticeSize = 1.5f;  // text scale (size) for notice
 	public static float spoutTerritoryNoticeLeaveAfterSeconds = 2.00f;  // how many seconds before the notice goes away
 	
@@ -192,7 +192,7 @@ public class Conf
 	public static boolean econEnabled = false;
 	public static String econUniverseAccount = "";
 	public static double econCostClaimWilderness = 30.0;
-	public static double econCostClaimFromFactionBonus = 30.0;
+	public static double econCostClaimFromguildBonus = 30.0;
 	public static double econClaimAdditionalMultiplier = 0.5;
 	public static double econClaimRefundMultiplier = 0.7;
 	public static double econClaimUnconnectedFee = 0.0;
@@ -216,11 +216,11 @@ public class Conf
 	public static double econCostNeutral = 0.0;
 	public static double econCostEnemy = 0.0;
 	
-	//Faction banks, to pay for land claiming and other costs instead of individuals paying for them
+	//guild banks, to pay for land claiming and other costs instead of individuals paying for them
 	public static boolean bankEnabled = true;
-	//public static boolean bankMembersCanWithdraw = false; //Have to be at least moderator to withdraw or pay money to another faction
-	public static boolean bankFactionPaysCosts = true; //The faction pays for faction command costs, such as sethome
-	public static boolean bankFactionPaysLandCosts = true; //The faction pays for land claiming costs.
+	//public static boolean bankMembersCanWithdraw = false; //Have to be at least moderator to withdraw or pay money to another guild
+	public static boolean bankguildPaysCosts = true; //The guild pays for guild command costs, such as sethome
+	public static boolean bankguildPaysLandCosts = true; //The guild pays for land claiming costs.
 
 	// mainly for other plugins/mods that use a fake player to take actions, which shouldn't be subject to our protections
 	public static Set<String> playersWhoBypassAllProtection = new LinkedHashSet<String>();
@@ -228,7 +228,7 @@ public class Conf
 	public static Set<String> worldsNoClaiming = new LinkedHashSet<String>();
 	public static Set<String> worldsNoPowerLoss = new LinkedHashSet<String>();
 	public static Set<String> worldsIgnorePvP = new LinkedHashSet<String>();
-	// TODO: A better solution Would be to have One wilderness faction per world.
+	// TODO: A better solution Would be to have One wilderness guild per world.
 	//public static Set<String> worldsNoWildernessProtection = new LinkedHashSet<String>();
 	
 	public static transient int mapHeight = 8;
@@ -239,16 +239,16 @@ public class Conf
 	{
 		baseCommandAliases.add("f");
 		
-		factionFlagDefaults = new LinkedHashMap<FFlag, Boolean>();
+		guildFlagDefaults = new LinkedHashMap<FFlag, Boolean>();
 		for (FFlag flag : FFlag.values())
 		{
-			factionFlagDefaults.put(flag, flag.defaultDefaultValue);
+			guildFlagDefaults.put(flag, flag.defaultDefaultValue);
 		}
 		
-		factionPermDefaults = new LinkedHashMap<FPerm, Set<Rel>>();
+		guildPermDefaults = new LinkedHashMap<FPerm, Set<Rel>>();
 		for (FPerm perm: FPerm.values())
 		{
-			factionPermDefaults.put(perm, perm.defaultDefaultValue);
+			guildPermDefaults.put(perm, perm.defaultDefaultValue);
 		}
 		
 		chatSingleFormats.put("pl", " %s");
