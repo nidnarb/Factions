3f16e32b649eadf8926a64a4c286cbd488c4354b
nidnarb <nidnarb@gmail.com>
2012-05-24 21:17:48 -0700

    Update src/com/massivecraft/factions/cmd/CmdMoneyTransferFf.java

--- src/com/massivecraft/factions/cmd/CmdMoneyTransferFf.java
+++ src/com/massivecraft/factions/cmd/CmdMoneyTransferFf.java
@@ -1,10 +1,10 @@
-package com.massivecraft.factions.cmd;
+package com.massivecraft.guilds.cmd;
 
-import com.massivecraft.factions.Conf;
-import com.massivecraft.factions.iface.EconomyParticipator;
-import com.massivecraft.factions.P;
-import com.massivecraft.factions.integration.Econ;
-import com.massivecraft.factions.struct.Permission;
+import com.massivecraft.guilds.Conf;
+import com.massivecraft.guilds.iface.EconomyParticipator;
+import com.massivecraft.guilds.P;
+import com.massivecraft.guilds.integration.Econ;
+import com.massivecraft.guilds.struct.Permission;
 
 import org.bukkit.ChatColor;
 
@@ -16,8 +16,8 @@ public class CmdMoneyTransferFf extends FCommand
 		this.aliases.add("ff");
 		
 		this.requiredArgs.add("amount");
-		this.requiredArgs.add("faction");
-		this.requiredArgs.add("faction");
+		this.requiredArgs.add("guild");
+		this.requiredArgs.add("guild");
 		
 		//this.optionalArgs.put("", "");
 		
@@ -34,14 +34,14 @@ public class CmdMoneyTransferFf extends FCommand
 	public void perform()
 	{
 		double amount = this.argAsDouble(0, 0d);
-		EconomyParticipator from = this.argAsFaction(1);
+		EconomyParticipator from = this.argAsguild(1);
 		if (from == null) return;
-		EconomyParticipator to = this.argAsFaction(2);
+		EconomyParticipator to = this.argAsguild(2);
 		if (to == null) return;
 		
 		boolean success = Econ.transferMoney(fme, from, to, amount);
 
 		if (success && Conf.logMoneyTransactions)
-			P.p.log(ChatColor.stripColor(P.p.txt.parse("%s transferred %s from the faction \"%s\" to the faction \"%s\"", fme.getName(), Econ.moneyString(amount), from.describeTo(null), to.describeTo(null))));
+			P.p.log(ChatColor.stripColor(P.p.txt.parse("%s transferred %s from the guild \"%s\" to the guild \"%s\"", fme.getName(), Econ.moneyString(amount), from.describeTo(null), to.describeTo(null))));
 	}
 }
